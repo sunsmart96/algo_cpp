095f6f0391b91482952d5bf2a39f975d933daf9d
sunsmart96 <sunsmart96@gmail.com>
2024-09-27 11:39:05 +0800

    add gtest example

--- algo_cpp/src/main.cpp
+++ algo_cpp/src/main.cpp
@@ -1,8 +1,17 @@
+#include <gtest/gtest.h>  // googletest header file
+
 #include <iostream>
+#include <string>
 
 using namespace std;
+using std::string;
 
-int main(int argc, char** argv) {
-  cout << "hello world!" << endl;
-  return 0;
+const char *actualValTrue = "hello gtest";
+const char *expectVal = "hello gtest";
+
+TEST(StrCompare, CStrEqual) { EXPECT_STREQ(expectVal, actualValTrue); }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- algo_cpp/xmake.lua
+++ algo_cpp/xmake.lua
@@ -1,8 +1,12 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires("gtest")
+
+
 target("algo_cpp")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_packages("gtest")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
