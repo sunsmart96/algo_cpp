3a4fc50526aaec1d53bcd99f701f01d2376ac85e
sunsmart96 <sunsmart96@gmail.com>
2024-10-02 02:21:02 +0800

    implement the FIFO_QUEUE

--- algo_cpp/src/algo.h
+++ algo_cpp/src/algo.h
@@ -1,6 +1,8 @@
 #ifndef ALGO_H
 #define ALGO_H
 #include <cstdint>
+#include <iostream>
+#include <memory>
 #include <vector>
 
 template <typename T> void swap(T &a, T &b) {
@@ -41,6 +43,73 @@ public:
 
 private:
   std::vector<T> v;
+};
+
+template <typename T> struct Node {
+
+  Node(T n) {
+    this->p_backward = nullptr;
+    this->value = n;
+  }
+
+  ~Node() { this->p_backward = nullptr; }
+  std::shared_ptr<Node<T>> p_backward;
+  T value;
+};
+
+template <typename T> class FIFO_QUEUE {
+public:
+  FIFO_QUEUE() {
+    n = 0;
+    p_head = nullptr;
+  }
+  void enqueue(std::shared_ptr<T> node) {
+    if (p_head == nullptr)
+      p_head = node;
+    else {
+      std::shared_ptr<T> p = p_head;
+      while (p.get()->p_backward != nullptr) {
+        p = p.get()->p_backward;
+      }
+      p.get()->p_backward = node;
+    }
+
+    n = n + 1;
+  }
+
+  bool is_empty() { return n == 0 ? true : false; }
+  size_t size() { return n; }
+
+  std::shared_ptr<T> dequeue() {
+    if (this->is_empty())
+      return nullptr;
+    else if (n > 1) {
+      std::shared_ptr<T> p = p_head;
+      for (int i = 0; i < n - 2; i++)
+        p = p.get()->p_backward;
+      std::shared_ptr<T> res = p.get()->p_backward;
+      p.get()->p_backward = nullptr;
+      n = n - 1;
+      return res;
+    } else if (n == 1) {
+      std::shared_ptr<T> res = this->p_head;
+      p_head = nullptr;
+      n = n - 1;
+      return res;
+    }
+  }
+
+  void print() const {
+    std::shared_ptr<T> p = p_head;
+    for (size_t i = 0; i < n; i++) {
+      std::cout << p.get()->value << std::endl;
+      p = p.get()->p_backward;
+    }
+  }
+
+private:
+  size_t n;
+  std::shared_ptr<T> p_head;
 };
 
 #endif
--- algo_cpp/src/algo_test.cpp
+++ algo_cpp/src/algo_test.cpp
@@ -10,18 +10,18 @@ const char *expectVal = "hello gtest";
 TEST(StrCompare, CStrEqual) { EXPECT_STREQ(expectVal, actualValTrue); }
 
 // test gcd algorithm
-TEST(TESTGCD, gcd_base1) {
+TEST(TESTGCD, GCD_BASE1) {
   EXPECT_EQ(10, gcd(10, 10));
   EXPECT_EQ(1, gcd(1, 10));
 }
 
-TEST(TESTGCD, gcd_base2) {
+TEST(TESTGCD, GCD_BASE2) {
   EXPECT_EQ(1, gcd(10, 3));
   EXPECT_EQ(2, gcd(10, 2));
 }
 
 // test binary search algorithm
-TEST(TEST_BINARY_SEARCH, binary_search) {
+TEST(TEST_BINARY_SEARCH, BINARY_SEARCH) {
   std::vector<int> v = {1, 2, 3, 3, 4, 5, 6, 9, 12};
 
   auto res1 = binary_search(v, 1);
@@ -43,7 +43,7 @@ TEST(TEST_BINARY_SEARCH, binary_search) {
 }
 
 // test Bag data structure
-TEST(TEST_BAG, base_test) {
+TEST(TEST_BAG, BASE_TEST) {
   auto bag = Bag<double>();
   EXPECT_EQ(0, bag.size());
   EXPECT_EQ(true, bag.is_empty());
@@ -58,6 +58,27 @@ TEST(TEST_BAG, base_test) {
     s = s + x;
 
   EXPECT_EQ(3.0, s);
+}
+
+TEST(TEST_FIFO_QUEUE, BASE_TEST) {
+  auto n1 = std::make_shared<Node<int>>(1);
+  auto n2 = std::make_shared<Node<int>>(2);
+  auto n3 = std::make_shared<Node<int>>(3);
+
+  auto q = std::make_shared<FIFO_QUEUE<Node<int>>>();
+  q->enqueue(n1);
+  q->enqueue(n2);
+  q->enqueue(n3);
+
+  EXPECT_EQ(3, q->size());
+
+  // q->print();
+
+  auto x = q->dequeue();
+
+  EXPECT_EQ(3, x.get()->value);
+
+  EXPECT_EQ(2, q->size());
 }
 
 int run_all_tests(int argc, char **argv) {
