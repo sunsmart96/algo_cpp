52012e3026c4aefe2255ef9c6690d0dc5e7ab185
sunsmart96 <sunsmart96@gmail.com>
2024-10-02 19:04:36 +0800

    implement Matrix class

--- algo_cpp/src/matrix_ops.h
+++ algo_cpp/src/matrix_ops.h
@@ -1,4 +1,65 @@
 #ifndef MATRIX_OPS_H
 #define MATRIX_OPS_H
 
+#include <cassert>
+#include <memory>
+
+template <typename T> void Matrix_Deleter(T **ptr, size_t rows) {
+  for (int i = 0; i < rows; ++i) {
+    delete[] ptr[i];
+  }
+  delete[] ptr;
+}
+
+template <typename T> class Matrix {
+public:
+  Matrix(size_t rows, size_t cols) {
+    this->cols = cols;
+    this->rows = rows;
+
+    std::shared_ptr<int *> p_2d_array(
+        new int *[rows], [rows](int **ptr) { Matrix_Deleter(ptr, rows); });
+
+    this->p = p_2d_array;
+    for (int i = 0; i < rows; ++i) {
+      this->p.get()[i] = new T[cols];
+    }
+  }
+
+  void set_all_value(T value) {
+    for (size_t i = 0; i < this->rows; i++) {
+      for (size_t j = 0; j < this->cols; j++) {
+        this->p.get()[i][j] = value;
+      }
+    }
+  }
+
+  void print() const {
+    for (size_t i = 0; i < rows; i++) {
+      for (size_t j = 0; j < cols; j++) {
+        std::cout << this->p.get()[i][j] << "   ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  void set_value_by_ij(size_t i, size_t j, T value) {
+    assert(i >= 0 && i < rows);
+    assert(j >= 0 && j < cols);
+
+    this->p.get()[i][j] = value;
+  }
+
+  T get_value_by_ij(size_t i, size_t j) {
+    assert(i >= 0 && i < rows);
+    assert(j >= 0 && j < cols);
+    return p.get()[i][j];
+  }
+
+private:
+  size_t cols;
+  size_t rows;
+  std::shared_ptr<T *> p;
+};
+
 #endif
--- algo_cpp/src/matrix_ops_test.cpp
+++ algo_cpp/src/matrix_ops_test.cpp
@@ -1,4 +1,14 @@
 #include "matrix_ops_test.h"
+#include "matrix_ops.h"
 #include <gtest/gtest.h> // googletest header file
 
-TEST(TEST_MATRIX, BASE_TEST) { EXPECT_EQ(3, 3); }
+TEST(TEST_MATRIX, BASE_TEST) {
+  EXPECT_EQ(3, 3);
+  auto m = Matrix<int>(3, 3);
+  m.set_all_value(3);
+  // m.print();
+  m.set_value_by_ij(2, 2, 10);
+  // m.print();
+  EXPECT_EQ(3, m.get_value_by_ij(0, 2));
+  EXPECT_EQ(10, m.get_value_by_ij(2, 2));
+}
